61a60fc6c3dce0352702944cc03178c8ad49f295
hyewonbae <bhwn98@gmail.com>
2024-08-16 21:18:53 +0900

    20240816 프로젝트 생성

---
+++ redbox/src/main/java/kr/co/ureca/redbox/RedboxApplication.java
@@ -0,0 +1,13 @@
+package kr.co.ureca.redbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedboxApplication.class, args);
+	}
+
+}
